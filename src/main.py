2b44485878958f323107e760a3427cbd9b3e9639
Mac Booey <macbooey@Macs-MacBook-Pro.local>
2023-10-27 20:35:17 -0700

    Module 1 question 10

--- src/main.py
+++ src/main.py
@@ -97,6 +97,7 @@ else:
     regular_pay = 40 * hourly_rate
     overtime_pay = (hours_worked - 40) * (hourly_rate * 1.5)
 
+# Question 7
 # Calculate the total weekly pay
 weekly_pay = regular_pay + overtime_pay
 
@@ -104,3 +105,34 @@ weekly_pay = regular_pay + overtime_pay
 print(f"Regular Pay: ${regular_pay:.2f}")
 print(f"Overtime Pay: ${overtime_pay:.2f}")
 print(f"Weekly Pay: ${weekly_pay:.2f}")
+
+# Question 8
+# Ask the user for their name
+user_name = input("Enter your name: ")
+
+# Display a welcome message
+print(f"Welcome, {user_name}!")
+
+# Question 9
+# Ask the user for inputs
+adjective = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+past_tense_verb = input("Enter a past-tense verb: ")
+noun2 = input("Enter another noun: ")
+
+# Create and display the mad lib phrase
+mad_lib_phrase = f"The quick, {adjective} {noun1} {past_tense_verb} over the lazy {noun2}."
+print(mad_lib_phrase)
+
+# Question 10
+# Ask the user for inputs
+adjective = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+past_tense_verb = input("Enter a past-tense verb: ")
+adverb = input("Enter an adverb: ")
+adjective2 = input("Enter an adjective: ")
+noun2 = input("Enter a noun: ")
+
+#madlib story
+my_mad_lib_phrase = f"Today I went to the zoo. I saw a(n) {adjective} {noun1} jumping up and down in its tree. He {past_tense_verb} {adverb} through the large tunnel that lead to its {adjective2} {noun2}."
+print(my_mad_lib_phrase)
