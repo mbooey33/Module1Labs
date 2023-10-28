22e8e434fa8cdaf43ee3a0c859f4602e9d6893bc
Mac Booey <macbooey@Macs-MacBook-Pro.local>
2023-10-27 19:49:02 -0700

    Module 1

--- src/main.py
+++ src/main.py
@@ -8,16 +8,16 @@ yearWhitworth = 1890
 ageWhitworth = 2023 - yearWhitworth
 print('Whitworth is ' + str(ageWhitworth) + ' years old')
 
-# question 2
+# question 1
 # Integer variables
-int_var1 = 15
-int_var2 = 7
-int_var3 = 20
+int_var1 = int(input("Enter an integer value for int_var1: "))
+int_var2 = int(input("Enter an integer value for int_var2: "))
+int_var3 = int(input("Enter an integer value for int_var3: "))
 
 # Float variables
-float_var1 = 10.5
-float_var2 = 3.2
-float_var3 = 8.0
+float_var1 = float(input("Enter a float value for float_var1: "))
+float_var2 = float(input("Enter a float value for float_var2: "))
+float_var3 = float(input("Enter a float value for float_var3: "))
 
 # Perform arithmetic operations on integers
 int_addition = int_var1 + int_var2
@@ -47,27 +47,33 @@ print("Multiplication:", float_multiplication)
 print("Division:", float_division)
 
 
-# question 2
-# Bill amount
-bill_amount = 47.56
+# Get the bill amount from the user
+bill_amount = float(input("Enter the bill amount: $"))
 
 # Tip percentages
 tip_percentages = [0.10, 0.15, 0.20, 0.25]
 
-# Calculate tip and total for each percentage
+# Calculate and display tip and total for each predetermined percentage
 for tip_percentage in tip_percentages:
     tip = bill_amount * tip_percentage
     total = bill_amount + tip
     print(f'Tip ({tip_percentage * 100}%): ${tip:.2f}, Total Bill: ${total:.2f}')
 
+# Ask the user for a custom tip percentage
+custom_tip_percentage = float(input("Enter your custom tip percentage (e.g., 18% as 0.18): "))
 
-# question 3
-# Point totals for each game
-game1 = 13
-game2 = 18
-game3 = 21
-game4 = 17
-game5 = 31
+# Calculate and display tip and total for the custom percentage
+custom_tip = bill_amount * custom_tip_percentage
+custom_total = bill_amount + custom_tip
+print(f'Custom Tip ({custom_tip_percentage * 100}%): ${custom_tip:.2f}, Total Bill: ${custom_total:.2f}')
+
+
+# Initialize variables to hold game point totals
+game1 = float(input("Enter the points for game 1: "))
+game2 = float(input("Enter the points for game 2: "))
+game3 = float(input("Enter the points for game 3: "))
+game4 = float(input("Enter the points for game 4: "))
+game5 = float(input("Enter the points for game 5: "))
 
 # Calculate the total points
 total_points = game1 + game2 + game3 + game4 + game5
@@ -77,4 +83,24 @@ num_games = 5  # Total number of games
 average_points_per_game = total_points / num_games
 
 # Print the average points per game
-print(f"Diana's average points per game: {average_points_per_game}")
+print(f"Average points per game: {average_points_per_game:.2f}")
+
+# Define hourly rate and number of hours worked
+hourly_rate = 16.5  # Dollar amount per hour
+hours_worked = float(input("Enter the number of hours worked this week: "))
+
+# Calculate the regular pay and overtime pay
+if hours_worked <= 40:
+    regular_pay = hours_worked * hourly_rate
+    overtime_pay = 0.0
+else:
+    regular_pay = 40 * hourly_rate
+    overtime_pay = (hours_worked - 40) * (hourly_rate * 1.5)
+
+# Calculate the total weekly pay
+weekly_pay = regular_pay + overtime_pay
+
+# Display the results
+print(f"Regular Pay: ${regular_pay:.2f}")
+print(f"Overtime Pay: ${overtime_pay:.2f}")
+print(f"Weekly Pay: ${weekly_pay:.2f}")
